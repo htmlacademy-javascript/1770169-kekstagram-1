82f95ddfb698f4a8af840df5dd3ba95044056478
kaplya-sys <strim.sv2010@gmail.com>
2023-09-28 22:59:53 +0500

    added generateRandomNumber function

--- js/main.js
+++ js/main.js
@@ -58,3 +58,11 @@ const generateNumber = () => {
 
 const getNumber = generateNumber();
 const getId = generateNumber();
+
+const generateRandomNumber = (min, max) => {
+  const minNumber = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const maxNumber = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const randomNumber = Math.random() * (maxNumber - minNumber + 1) + minNumber;
+
+  return Math.floor(randomNumber);
+};
