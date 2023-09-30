126d09c86b105c599481ec5a697201e406afd5d4
kaplya-sys <strim.sv2010@gmail.com>
2023-09-30 13:21:42 +0500

    divided the data into modules

---
+++ js/utils.js
@@ -0,0 +1,20 @@
+const generateNumber = () => {
+  let number = 0;
+
+  return function() {
+    number += 1;
+    return number;
+  };
+};
+
+const generateRandomNumber = (min, max) => {
+  const minNumber = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const maxNumber = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const randomNumber = Math.random() * (maxNumber - minNumber + 1) + minNumber;
+
+  return Math.floor(randomNumber);
+};
+
+const getRandomElement = (elements) => elements[generateRandomNumber(0, elements.length - 1)];
+
+export {generateNumber, generateRandomNumber, getRandomElement};
