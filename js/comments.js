6f4a26f20423711cfbfe022b6e0783a9c64e5f7c
kaplya-sys <strim.sv2010@gmail.com>
2023-10-05 21:46:34 +0500

    added functionality for displaying part of the comment

--- js/comments.js
+++ js/comments.js
@@ -1,8 +1,8 @@
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 const commentsElement = document.querySelector('.social__comments');
 const commentsCountElement = document.querySelector('.comments-count');
-const socialCommentCountElement = document.querySelector('.social__comment-count');
 const commentsLoaderElement = document.querySelector('.comments-loader');
+const currentComments = document.querySelector('.current-comments');
 
 const getCommentElement = ({avatar, message, name}) => {
   const commentElement = commentTemplate.cloneNode(true);
@@ -15,15 +15,19 @@ const getCommentElement = ({avatar, message, name}) => {
   return commentElement;
 };
 
-export const renderComments = (comments) => {
+export const renderComments = (comments, countComments) => {
   const fragment = document.createDocumentFragment();
+  const minComments = Math.min(comments.length, countComments);
 
-  for (let i = 0; i < comments.length; i++) {
+  for (let i = 0; i < minComments; i++) {
     fragment.append(getCommentElement(comments[i]));
   }
   commentsCountElement.textContent = comments.length;
-  socialCommentCountElement.classList.add('hidden');
-  commentsLoaderElement.classList.add('hidden');
+  currentComments.textContent = minComments;
+
+  if (comments.length <= countComments) {
+    commentsLoaderElement.classList.add('hidden');
+  }
   commentsElement.innerHTML = '';
   commentsElement.append(fragment);
 };
